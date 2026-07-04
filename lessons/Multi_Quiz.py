5f9b76bad90c6e1610cd6f16d06124ac2b9aa32c
Arjunnamas <arjun.namas.business@gmail.com>
2026-07-05 00:57:53 +0100

    question

--- lessons/Multi_Quiz.py
+++ lessons/Multi_Quiz.py
@@ -1,6 +1,25 @@
-questions = [
+from Question import question
+
+question_prompts = [
 "What colour are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
 "What Colour are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
 "What colour are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
 
 ]
+
+questions = [
+    question(question_prompts[0], "a"),
+    question(question_prompts[1], "c"),
+    question(question_prompts[2], "b"),
+
+]
+
+def run_quiz(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("you got " + str(score) +"/"+ str(len(questions)) + " correct")
+
+run_quiz(questions)
