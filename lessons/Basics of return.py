b762c603490dba0682f0592255b9bc2827af9df0
Arjunnamas <arjun.namas.business@gmail.com>
2026-07-06 00:56:31 +0100

    basics of return

---
+++ lessons/Basics of return.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+    return a + b #to use a result later
+    #print(a + b) to show result
+
+
+
+result = add_numbers(3,4)
+
+
+print(result)
+
+result = result + 3
+
+print(result)
